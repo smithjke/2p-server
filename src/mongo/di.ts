b54fb972a8fa70bce3284b188455f1b64ca6a0cb
smithjke <smithjke@gmail.com>
2022-09-30 00:09:46 +0300

    feat: add code

---
+++ src/mongo/di.ts
@@ -0,0 +1,4 @@
+// import TPCore from '2p-core';
+// import { MongoService } from './mongo.service';
+//
+// export const useMongoService = () => TPCore.di.getDependency<MongoService>('MONGO_SERVICE');
